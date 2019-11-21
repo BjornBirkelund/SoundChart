6929100bb943561eecf11ea0a81243924f6aa0d7
BjornBirkelund <bbirkelund@gmail.com>
2019-11-21 21:27:18 +0100

    Attempt to fix update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
-import * as Charts from './components/charts';
-import { Button,ButtonGroup,ButtonToolbar,ToggleButtonGroup,ToggleButton,Row,Col, Container } from 'react-bootstrap';
-import './buttonGroup.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as Charts from "./components/charts";
+import {
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
+  ToggleButtonGroup,
+  ToggleButton,
+  Row,
+  Col,
+  Container
+} from "react-bootstrap";
+import "./buttonGroup.css";
 
 const b1Style = {
   // backgroundColor: 'rgba(255,255,255,0)',
-  borderColor: 'white',
-  borderWidth: '2px',
-  borderRight: 'none'
+  borderColor: "white",
+  borderWidth: "2px",
+  borderRight: "none"
 };
 const b2Style = {
   // backgroundColor: 'rgba(255,255,255,0)',
-  borderColor: 'white',
-  borderWidth: '2px',
-  borderLeft: 'none',
-  borderRight: 'none'
+  borderColor: "white",
+  borderWidth: "2px",
+  borderLeft: "none",
+  borderRight: "none"
 };
 const b3Style = {
   // backgroundColor: 'rgba(255,255,255,0)',
-  borderColor: 'white',
-  borderWidth: '2px',
-  borderLeft: 'none',
-  borderRight: 'none'
-
+  borderColor: "white",
+  borderWidth: "2px",
+  borderLeft: "none",
+  borderRight: "none"
 };
 const b4Style = {
   // backgroundColor: 'rgba(255,255,255,0)',
-  borderColor: 'white',
-  borderWidth: '2px',
-  borderLeft: 'none'
+  borderColor: "white",
+  borderWidth: "2px",
+  borderLeft: "none"
 };
 
 const titleStyle = {
   color: "white",
   fontFamily: "PT Sans",
   fontWeight: "bold",
-  fontSize: "200%",
+  fontSize: "200%"
 };
 
 class App extends Component {
   state = {
-    selectedChartType: 'DayChart',
+    selectedChartType: "DayChart",
     soundlevels: {
-      'DayChart': [ 10, 23, 30, 40,  50, 80, 12, 12,  12, 44, 55, 66 ],
+      DayChart: [10, 23, 30, 40, 50, 80, 12, 12, 12, 44, 55, 66]
       //...
     }
   };
@@ -56,47 +64,95 @@ class App extends Component {
     setInterval(this.updateSoundLevels.bind(this), 2000);
   }
 
-  updateSoundLevels( ) {
-      console.log("update");
-      let newSoundLevels = this.state.soundlevels['DayChart'].map((sl) => sl*2);
+  updateSoundLevels = () => {
+    console.log("update");
+    let newSoundLevels = this.state.soundlevels["DayChart"].map(sl => sl * 2);
 
-      this.setState({'soundlevels': { 'DayChart':  newSoundLevels }});
-  }
-  
-  render(){
+    this.setState({ soundlevels: { DayChart: newSoundLevels } });
+  };
+  handleUpdateData = chart => {
+    let myData = [...this.state.soundlevels.DayChart];
+
+    // chart.data.labels.push(label);
+    // chart.data.datasets.forEach(dataset => {
+    //   dataset.data.push(myData);
+    // });
+    // chart.update();
+    // chart.datasets[0] = myData;
+    // chart.update();
+    console.log("g data", myData, chart);
+  };
+
+  render() {
     return (
-      
       <div className="App">
-        <header className="App-header"> 
-          <Container>    
+        <header className="App-header">
+          <Container>
             <Row>
               <Col xs={6} text-align-left>
                 <h4 style={titleStyle}>Decibel Levels</h4>
               </Col>
               <Col col2 my-auto text-center xs={6}>
-                {this.renderTimeSelector(b1Style,b2Style,b3Style,b4Style)}
+                {this.renderTimeSelector(b1Style, b2Style, b3Style, b4Style)}
               </Col>
-            </Row> 
+            </Row>
           </Container>
 
-              {this.renderSelectedChart(this.state.selectedChartType)}
+          {this.renderSelectedChart(this.state.selectedChartType)}
         </header>
       </div>
     );
   }
 
-  renderTimeSelector(b1Style,b2Style,b3Style,b4Style) {
+  renderTimeSelector(b1Style, b2Style, b3Style, b4Style) {
     return (
       <ButtonToolbar>
-        <ToggleButtonGroup size='lg' type="radio" name="options" defaultValue={1}>
-          <ToggleButton value={1} style={b1Style} variant="outline-light"
-          onClick={(e) => {this.setState({ selectedChartType: "DayChart"})}}>Day</ToggleButton>
-          <ToggleButton value={2} style={b2Style} variant="outline-light"
-          onClick={(e) => {this.setState({ selectedChartType: "WeekChart"})}}>Week</ToggleButton>
-          <ToggleButton value={3} style={b3Style} variant="outline-light"
-          onClick={(e) => {this.setState({ selectedChartType: "MonthChart"})}}>Month</ToggleButton>
-          <ToggleButton value={4} style={b4Style} variant="outline-light"
-          onClick={(e) => {this.setState({ selectedChartType: "YearChart"})}}>Year</ToggleButton>
+        <ToggleButtonGroup
+          size="lg"
+          type="radio"
+          name="options"
+          defaultValue={1}
+        >
+          <ToggleButton
+            value={1}
+            style={b1Style}
+            variant="outline-light"
+            onClick={e => {
+              this.setState({ selectedChartType: "DayChart" });
+            }}
+          >
+            Day
+          </ToggleButton>
+          <ToggleButton
+            value={2}
+            style={b2Style}
+            variant="outline-light"
+            onClick={e => {
+              this.setState({ selectedChartType: "WeekChart" });
+            }}
+          >
+            Week
+          </ToggleButton>
+          <ToggleButton
+            value={3}
+            style={b3Style}
+            variant="outline-light"
+            onClick={e => {
+              this.setState({ selectedChartType: "MonthChart" });
+            }}
+          >
+            Month
+          </ToggleButton>
+          <ToggleButton
+            value={4}
+            style={b4Style}
+            variant="outline-light"
+            onClick={e => {
+              this.setState({ selectedChartType: "YearChart" });
+            }}
+          >
+            Year
+          </ToggleButton>
         </ToggleButtonGroup>
       </ButtonToolbar>
     );
@@ -105,11 +161,8 @@ class App extends Component {
   renderSelectedChart(selectedChartType) {
     const Chart = Charts[selectedChartType];
     const data = this.state.soundlevels[selectedChartType];
-    return <Chart data={data}/>
+    return <Chart data={data} onDataUpdate={this.handleUpdateData} />;
   }
-
 }
-
-  
 
 export default App;
--- src/components/charts/DayChart.js
+++ src/components/charts/DayChart.js
@@ -1,78 +1,92 @@
-import React, { Component } from 'react'
-import Chart from "chart.js";
+import React, { Component } from "react";
+import { Chart } from "react-chartjs-2";
+//import Chart from "chart.js";
 import classes from "./Chart.module.css";
 
-
 class BarGraph extends Component {
-    chartRef = React.createRef();
-    
-    constructor() {
-        super();
-        this.chart = null;
-    }
+  chartRef = React.createRef();
 
-    componentDidMount() {
-        const myChartRef = this.chartRef.current.getContext("2d");
-        
-        let myData = this.props.data;
+  constructor() {
+    super();
+    // this.chart = null;
+  }
 
-        this.chart = new Chart(myChartRef, {
-            type: "bar",
-            data: {
-                //Bring in data
-                labels: ["12AM","2AM","4AM","6AM", "8AM", "10AM","12PM", "2PM", "4PM","6PM","8PM","10PM","12PM"],
-                datasets: [
-                    {
-                        label: "", // `Mean: ${Math.round(myDataMean)}`,
-                        data: myData,
-                        backgroundColor: 'rgba(255,52,33,.6)'     
-                    }
-                ]
-            },
-            options: {
-                maintainAspectRatio: false 
-                // ,title: {
-                //     display: true,
-                //     text:'Decibel level for various sounds',
-                //     fontSize:25
-                // }
-                ,legend: {
-                    labels: {
-                        fontStyle: 'bold'
-                    },
-                    display: true,
-                    position:'top'
-                }
-                ,scales: {
-                    yAxes: [{
-                        ticks: {
-                            max: 140,
-                            min: 0 
-                        },
-                    }]
-                }
-                //Customize chart options
+  componentDidMount() {
+    const myChartRef = this.chartRef.current.getContext("2d");
+
+    let myData = this.props.data;
+
+    this.chart = new Chart(myChartRef, {
+      type: "bar",
+      data: {
+        //Bring in data
+        labels: [
+          "12AM",
+          "2AM",
+          "4AM",
+          "6AM",
+          "8AM",
+          "10AM",
+          "12PM",
+          "2PM",
+          "4PM",
+          "6PM",
+          "8PM",
+          "10PM",
+          "12PM"
+        ],
+        datasets: [
+          {
+            label: "", // `Mean: ${Math.round(myDataMean)}`,
+            data: myData,
+            backgroundColor: "rgba(255,52,33,.6)"
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        // ,title: {
+        //     display: true,
+        //     text:'Decibel level for various sounds',
+        //     fontSize:25
+        // }
+        legend: {
+          labels: {
+            fontStyle: "bold"
+          },
+          display: true,
+          position: "top"
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                max: 140,
+                min: 0
+              }
             }
-        });
-        console.log("made chart", this.chart);
-    }
-
-    render() {
-        var {data} = this.props;
-        console.log("got data", data, this.chart);
-        if (this.chart) {
-            this.chart.datasets[0].data = data;
-        } else {
-            
+          ]
         }
-        return (
-                <div className={classes.graphContainer}>
-                    <canvas
-                        id="myChart"
-                        ref={this.chartRef}
-                    />
-                </div>
-        )
-    }
+        //Customize chart options
+      }
+    });
+    console.log("made chart", this.chart);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps: ", prevProps);
+    console.log("prevState: ", prevState);
+    console.log("chart updated");
+  }
+
+  render() {
+    this.props.onDataUpdate(this.chart);
+    // const { data: myData } = this.props;
+    // console.log("got data", myData, this.chart);
+    return (
+      <div className={classes.graphContainer}>
+        <canvas id="myChart" ref={this.chartRef} />
+      </div>
+    );
+  }
 }
 export default BarGraph;
